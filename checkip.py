adffb79f0c1261509a8f8f641dc94bda633a7942
Hien Nguyen <hien_m_nguyen@yahoo.com>
2019-03-13 18:44:46 -0700

    another minor change

--- checkip.py
+++ checkip.py
@@ -5,19 +5,20 @@ Created on Fri Sep 22 09:50:20 2017
 """
 import re
 
+#find the matching words inside the input file
 def find_word(filename, word):
-    fo = open(filename, 'r')
-    res = re.findall(word, fo.read())
-    fo.close()
-    return res
+    with open(filename, 'r') as fo:
+        res = re.findall(word, fo.read())
+        fo.close()
+        return res
 
 def print_res(result):
     for item in result:
         print(item)
-    print('\n\n****Found {} entries'.format(len(result)))
+    print(f'\n\n****Found {len(result)} entries****')
     
 found = find_word('mbox-short.txt', r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-for x in found: 
-    print(x)
+print_res(found)
+
 
     
